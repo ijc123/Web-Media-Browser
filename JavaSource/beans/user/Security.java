93be4acc1ed873e259022d04ca6924630f6232d0
ic <i.coronel@hotmail.com>
2011-12-05 16:06:36 +0100

    added login security

--- JavaSource/beans/user/Security.java
+++ JavaSource/beans/user/Security.java
@@ -1,35 +1,55 @@
 package beans.user;
 
+import java.io.IOException;
+
 import javax.enterprise.event.Observes;
 import javax.faces.application.NavigationHandler;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.inject.Inject;
 
 import org.jboss.seam.faces.event.qualifier.After;
 import org.jboss.seam.faces.event.qualifier.Before;
-import org.jboss.seam.faces.event.qualifier.RenderResponse;
+import org.jboss.seam.faces.event.qualifier.RestoreView;
 
 public class Security {
 
 	@Inject
 	LoginBean loginSession;
 	
+	@Inject 
+	ExternalContext externalContext;
+	
+	@Inject
+	FacesContext facesContext;
+	
 	public void observeBefore(@Observes @Before PhaseEvent e)
 	{
-		// Listens to all phases, before the phase
-		if(loginSession != null) {
+	
+	}
+	 
+	public void observeAfter(@Observes @After @RestoreView PhaseEvent e, NavigationHandler navHandler)
+	{
+		String loginPage = "/pages/login.xhtml";
+		String redirectPage = "/mijngod/pages/login.jsf";
+		
+		// Listens to the RenderResponse phase, after the phase
+		boolean loggedIn = loginSession.isLoggedIn();
+		
+		String currentPage = facesContext.getViewRoot().getViewId();
+		
+		if(!loggedIn && !currentPage.equals(loginPage)) {
 			
-			boolean loggedIn = loginSession.isLoggedIn();
+			try {
+				externalContext.redirect(redirectPage);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			
 		}
 		
-	}
-	 
-	public void observeAfter(@Observes @After @RenderResponse PhaseEvent e, NavigationHandler navHandler)
-	{
-		// Listens to the RenderResponse phase, after the phase
-		
-	
 	}
 	
 }
